929fe656ccd5610bcc42c02b21c0c5d0ca8a921d
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-07-29 12:33:29 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map,
-            path, expected):
+            path, expected_result):
         """A function that tests
         access_nested_map"""
         try:
