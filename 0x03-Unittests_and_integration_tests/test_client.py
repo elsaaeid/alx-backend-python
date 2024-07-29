a996ea3142cbc139dfe9354bdbccf89e88c238d9
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-07-30 01:31:02 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -64,25 +64,49 @@ class TestGithubOrgClient(unittest.TestCase):
 
 @parameterized_class(['org_payload', 'repos_payload',
                       'expected_repos', 'apache2_repos'], TEST_PAYLOAD)
-class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """Class that tests Integration_github_org_client"""
-    @patch('client.GithubOrgClient.get_json')
-    def test_public_repos(self, mock_get_json):
-        """Function that tests public repos"""
-        mock_get_json.return_value = TEST_PAYLOAD
-        client = GithubOrgClient("google")
-        result = client.public_repos()
-        self.assertEqual(result, ["episodes.dart"])
-        mock_get_json.assert_called_once_with("https://api.github.com/orgs/google/repos")
 
-    @patch('client.GithubOrgClient.get_json')
-    def test_public_repos_with_license(self, mock_get_json):
-        """Function that tests public repos with license"""
-        mock_get_json.return_value = TEST_PAYLOAD
-        client = GithubOrgClient("google")
-        result = client.public_repos(license="apache-2.0")
-        self.assertEqual(result, [])
-        mock_get_json.assert_called_once_with("https://api.github.com/orgs/google/repos")
+
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ Class for Integration test of fixtures """
+
+    @classmethod
+    def setUpClass(cls):
+        """Class method called before tests in an individual class are run"""
+
+        attr = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **attr)
+
+        cls.mock = cls.get_patcher.start()
+
+    def test_public_repos(self):
+        """ Integration test: public repos"""
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """ Integration test for public repos with License """
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.assertEqual(test_class.public_repos(
+            "apache-2.0"), self.apache2_repos)
+        self.mock.assert_called()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Class method called after tests in an individual class have run"""
+        cls.get_patcher.stop()
 
 if __name__ == '__main__':
     unittest.main()
